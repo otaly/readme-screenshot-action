8f57f56884e105ffbbdaf3092b70d7b6dca1df04
otaly <60144056+otaly@users.noreply.github.com>
2024-08-03 01:30:26 +0900

    chromeをインストールする処理追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import { homedir } from 'node:os';
+import { join } from 'node:path';
 import * as core from '@actions/core';
+import {
+  Browser,
+  BrowserPlatform,
+  install,
+  resolveBuildId,
+} from '@puppeteer/browsers';
 import puppeteer from 'puppeteer';
 
 const run = async () => {
+  installChrome();
+
   const url = core.getInput('url');
 
   const browser = await puppeteer.launch();
@@ -11,6 +21,20 @@ const run = async () => {
   await page.screenshot({ path: 'screenshot.png' });
 
   await browser.close();
+};
+
+const installChrome = async () => {
+  const browser = Browser.CHROME;
+  const buildId = await resolveBuildId(
+    browser,
+    BrowserPlatform.LINUX,
+    'latest',
+  );
+  await install({
+    cacheDir: join(homedir(), '.cache', 'puppeteer'),
+    browser,
+    buildId,
+  });
 };
 
 run();
