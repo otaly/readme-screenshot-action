7cc188ec6ecc93c8a7235750cd2b3407482a848a
otaly <60144056+otaly@users.noreply.github.com>
2024-08-08 00:27:16 +0900

    タグがない場合は末尾に挿入

--- src/readme.ts
+++ src/readme.ts
@@ -6,6 +6,28 @@ export const updateReadme = (url: string, screenshotPath: string) => {
   const file = fs.readFileSync('README.md', { encoding: 'utf8' });
 
   const lines = file.split('\n');
+  const tag = findTag(lines);
+
+  if (tag.begin === -1 && tag.end === -1) {
+    lines.push(
+      '<!-- [README-SCREENSHOT-BEGIN] -->',
+      '<!-- [README-SCREENSHOT-END] -->',
+    );
+    tag.begin = lines.length - 2;
+    tag.end = lines.length - 1;
+  }
+
+  // 開始タグと終了タグの間を置換
+  lines.splice(
+    tag.begin + 1,
+    tag.end - tag.begin - 1,
+    `![${url}](${screenshotPath})`,
+  );
+
+  fs.writeFileSync('README.md', lines.join('\n'));
+};
+
+const findTag = (lines: string[]) => {
   const begin = lines.findIndex((l) =>
     /<!-- *\[README-SCREENSHOT-BEGIN\] *-->/.test(l),
   );
@@ -13,8 +35,5 @@ export const updateReadme = (url: string, screenshotPath: string) => {
     /<!-- \[README-SCREENSHOT-END\] -->/.test(l),
   );
 
-  // 開始タグと終了タグの間を置換
-  lines.splice(begin + 1, end - begin - 1, `![${url}](${screenshotPath})`);
-
-  fs.writeFileSync('README.md', lines.join('\n'));
+  return { begin, end };
 };
